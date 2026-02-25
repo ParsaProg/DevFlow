cbe52921879ec79ff524b898b1c32f43338e563e
ParsaProg <parsashaabani3@gmail.com>
2026-02-25 20:45:25 +0330

    HomePage | LanfingPage => page.tsx changes, remove pert datas

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,5 @@
 "use client";
 
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-  const [mounted, setMounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-  return (
-    <div className="w-full">
-      <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-        {theme === "dark" ? "Dark" : "Light"}
-      </button>
-    </div>
-  );
+  return <div className="w-full"></div>;
 }
