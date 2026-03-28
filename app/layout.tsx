da106dec653646b883bb11978becfe626c2747c1
ParsaProg <parsashaabani3@gmail.com>
2026-03-29 01:23:00 +0330

    layout.tsx > main changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,11 @@
 import ClientLayout from "./ClientLayout";
 import type { Metadata } from "next";
-import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LenisProvider from "@/src/utils/lenis-provider";
 
 const title: string = "DevFlow | Home";
 const description: string =
   "DevFlow - Modern Developer Collaboration Platform | Structured issue tracking, clean task management, and collaborative workspaces for developer teams.";
-
-const geist_mono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-geist-mono",
-});
 
 export const metadata: Metadata = {
   title: title,
@@ -30,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`transition-colors duration-500 bg-background text-for-default ${geist_mono.variable} antialiased`}
+        className={`inter transition-colors duration-500 bg-background text-for-default antialiased`}
       >
         <ClientLayout>
           <LenisProvider>{children}</LenisProvider>
