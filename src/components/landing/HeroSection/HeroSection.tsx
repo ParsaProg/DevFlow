246f18c0e788317da056448fa3e9902f4d218938
ParsaProg <parsashaabani3@gmail.com>
2026-03-29 01:23:44 +0330

    HeroSection update

--- src/components/landing/HeroSection/HeroSection.tsx
+++ src/components/landing/HeroSection/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
 
   return (
     <div className="relative h-[50vw] ">
-      <div className="absolute top-[50%] translate-y-[-50%] left-0 w-50 h-100 rounded-full bg-primary blur-[150px] opacity-20 -translate-x-20"></div>
+      <div className="absolute top-[50%] translate-y-[-50%] left-0 w-50 h-70 rounded-full bg-primary blur-[150px] opacity-40 -translate-x-20"></div>
       <div className="absolute top-[50%] translate-y-[-50%] right-0 w-50 h-100 rounded-full bg-purple-500 blur-[100px] opacity-10 translate-x-20"></div>
       <div className="absolute w-full text-center">
         <div className="w-full h-[50vw] flex aspect-video flex-col items-center justify-center">
@@ -21,7 +21,7 @@ const HeroSection = () => {
       </div>
       <div className="flex items-center min-[1300px]:w-325 justify-between absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] mx-auto ">
         <section className="w-[50%] flex flex-col items-start gap-y-8 ">
-          <div className="rounded-full dark:bg-[#1e232d47] bg-[#F1F3F7] border dark:border-neutral-800 border-neutral-700 text-neutral-600 dark:text-neutral-400 flex items-center py-2 px-4 gap-x-2 text-xs font-semibold backdrop-blur-sm">
+          <div className="rounded-full dark:bg-[#1e232d47] bg-[#F1F3F7] border dark:border-neutral-800 border-neutral-700 text-neutral-600 dark:text-neutral-400 flex items-center py-2 px-4 gap-x-2 text-xs backdrop-blur-sm">
             <div className="relative">
               <div className="rounded-full bg-[#00A9B3] w-2 h-2">
                 <div className="rounded-full bg-[#00A9B3] w-2 h-2 animate-ping"></div>
@@ -38,11 +38,11 @@ const HeroSection = () => {
             management, and team collaboration into one streamlined experience.
           </p>
           <div className="flex items-center gap-x-5 font-semibold mt-3">
-            <button className="transition-transform duration-300 hover:translate-y-[-5px] cursor-pointer rounded-2xl px-7 py-3 bg-primary text-white shadow-primary/70 shadow-[0px_0px_25px_0px] flex items-center gap-x-2">
+            <button className="transition-transform duration-300 hover:translate-y-[-3px] cursor-pointer rounded-2xl px-7 py-3 bg-primary text-white shadow-primary/70 shadow-[0px_0px_25px_0px] flex items-center gap-x-2 text-sm">
               Start Free
               <ArrowRight size={18} />
             </button>
-            <button className="transition-[transform_colors] dark:hover:bg-[#181b22] duration-300 hover:translate-y-[-5px] cursor-pointer border dark:border-neutral-800 rounded-2xl px-7 py-3 dark:text-white text-black dark:bg-[#0d10164e]">
+            <button className="transition-[transform_colors] dark:hover:bg-[#181b22] duration-300 hover:translate-y-[-3px] cursor-pointer border dark:border-neutral-800 rounded-2xl px-7 py-3 dark:text-white text-black dark:bg-[#1e232e4e] text-sm">
               Back Demo
             </button>
           </div>
