ae9c3046ea3d2b4582ad98b3d2c94f41ca15e2b6
ParsaProg <parsashaabani3@gmail.com>
2026-04-03 11:59:59 +0330

    page.tsx updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 
 import LandingFeatures from "@/src/components/landing/Features/Features";
 import HeroSection from "@/src/components/landing/HeroSection/HeroSection";
+import LandingTrustedSection from "@/src/components/landing/Trusted/TrustedSection";
 
 export default function Home() {
   return <div className="w-full">
     <HeroSection />
     <LandingFeatures />
-    
-    <div className="h-2500"></div>
+    <LandingTrustedSection /> 
   </div>;
 }
