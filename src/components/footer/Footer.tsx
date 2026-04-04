7e34ad1a608601f30fd593afc90fed78bdc2c6a0
ParsaProg <parsashaabani3@gmail.com>
2026-04-04 18:19:03 +0330

    Footer.tsx updates

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import DevFlow from "@/app/assets/pictures/devflow.png";
 
 export default function Footer() {
   return (
-    <div className="flex flex-col sm:flex-row items-center w-full justify-between gap-4 sm:gap-0 min-[1300px]:max-w-325 mx-auto border-t pb-6 sm:pb-8 pt-5 dark:border-t-gray-800 px-4 sm:px-6 lg:px-8">
+    <div className="flex flex-col sm:flex-row items-center w-full justify-between sm:gap-0 gap-3 min-[1300px]:w-325 mx-auto border-t pb-6 sm:pb-8 pt-5 dark:border-t-gray-800 px-4 sm:px-0">
       <div className="select-none flex items-center gap-x-2 font-bold">
         <div className="overflow-hidden text-white rounded-xl w-6 h-6 items-center flex justify-center">
           <Image
@@ -16,13 +16,13 @@ export default function Footer() {
         </div>
         <h1 className="text-sm sm:text-md font-semibold">DevFlow</h1>
       </div>
-      
-      <div className="flex flex-col sm:flex-row items-center gap-2 sm:gap-3 md:gap-4 order-last sm:order-none">
-        <span className="text-xs dark:text-gray-500 text-center">© 2026 all rights reserved</span>
-        <span className="text-xs dark:text-gray-500 text-center">Built for developers, by developers.</span>
-      </div>
-      
-      <div className="hidden sm:block w-16"></div>
+
+      <span className="text-xs dark:text-gray-500 text-center">
+        © 2026 all rights reserved
+      </span>
+      <span className="text-xs dark:text-gray-500 text-center">
+        Built for developers, by developers.
+      </span>
     </div>
   );
 }
