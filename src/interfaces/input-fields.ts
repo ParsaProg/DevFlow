46b5e34edb51c41d48ce2cd244ee9b48d99c1749
ParsaProg <parsashaabani3@gmail.com>
2026-04-12 17:06:52 +0330

    input-fields interface file added for structured my project!

---
+++ src/interfaces/input-fields.ts
@@ -0,0 +1,10 @@
+export interface InputFieldInterface {
+  fieldTitle: string;
+  placeHolder: string;
+  type: string;
+  mainIcon: React.ReactNode;
+  formAuthInput: string;
+  register: any;
+  errors: any;
+  setContent: (val: string) => void;
+}
