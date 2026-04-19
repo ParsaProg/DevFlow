d0aaf6ab401bda533ec6124272b034eb398de532
ParsaProg <parsashaabani3@gmail.com>
2026-04-19 15:15:25 +0330

    layout.tsx

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+import { ReactNode } from "react";
+
+const title = "DevFlow | Dashboard";
+const desc = "DevFlow dashboard page, main project managing system route!";
+
+export const metadata: Metadata = {
+  title: title,
+  description: desc,
+};
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
