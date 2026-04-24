8d919cc4949ba3d3366da7dfb5d6b24134f7c26c
ParsaProg <parsashaabani3@gmail.com>
2026-04-24 18:34:37 +0330

    TabComponent in the dashboard page

--- src/components/dashboard/TabComponent.tsx
+++ src/components/dashboard/TabComponent.tsx
@@ -1,13 +1,21 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import AuthPagesBrandShows from "../auth/BrandShows";
 import { useEffect, useState } from "react";
 import { DashboardTabList } from "@/src/data/dashboard-tab-component-list";
 import Link from "next/link";
+import { ChevronsLeft } from "lucide-react";
+import { TabComponentInterface } from "@/src/interfaces/tab-component-interface";
 
-export default function TabComponent() {
+export default function TabComponent({
+  collapse,
+  setCollapse,
+}: TabComponentInterface) {
+  const toggleCollapse = () => {
+    setCollapse(!collapse);
+  };
   const pathName = usePathname();
   const [mount, setMount] = useState<boolean | null>(null);
   useEffect(() => {
@@ -17,10 +25,15 @@ export default function TabComponent() {
 
   if (!mount) return null;
   return (
-    <div className="bg-[#080B10] z-20 fixed top-0 left-0 w-60 h-screen border-r border-r-gray-800">
+    <motion.div
+      transition={{ duration: 0.5 }}
+      className={`transition-all duration-300 shrink-0 bg-[#080B10] z-20 fixed top-0 left-0 ${collapse ? "w-15" : "w-60"} h-screen border-r border-r-gray-800 overflow-hidden`}
+    >
       <div className="relative h-16 w-full border-b border-b-gray-800 flex flex-col justify-center">
-        <div className="left-0 absolute scale-[0.8] mr-5">
-          <AuthPagesBrandShows />
+        <div
+          className={`absolute scale-[0.8] ${collapse ? "left-2" : "left-1 "}`}
+        >
+          <AuthPagesBrandShows showTitle={!collapse} />
         </div>
       </div>
       <div className="flex flex-col gap-y-1 w-[100%-12px] mx-3 mt-5">
@@ -36,10 +49,25 @@ export default function TabComponent() {
                     ? "/dashboard"
                     : `/dashboard/${val.title.toString().toLowerCase()}`
               }
-              className={`${pathName === "/dashboard" ? pathName === `/${val.title.toLowerCase()}` && "bg-gray-900 text-primary" : pathName === `/dashboard/${val.title.toLowerCase()}` ? "bg-gray-900 text-primary" : ""} px-3 py-2 text-[14.5px] text-gray-400 gap-x-2 relative rounded-xl w-full flex items-center transition-all duration-200 hover:bg-gray-900`}
+              className={`${pathName === "/dashboard" ? pathName === `/${val.title.toLowerCase()}` && "bg-gray-900 text-primary" : pathName === `/dashboard/${val.title.toLowerCase()}` ? "bg-gray-900 text-primary" : ""} ${collapse ? "p-2.25" : "px-3 py-2"} text-[14.5px] text-gray-400 gap-x-2 relative rounded-xl w-full flex items-center transition-all duration-200 hover:bg-gray-900`}
             >
               {val.icon}
-              <h1>{val.title}</h1>
+              <AnimatePresence>
+                {!collapse && (
+                  <motion.h1
+                    transition={{ delay: 0.1 }}
+                    key={"title"}
+                    initial="hidden"
+                    animate="visible"
+                    variants={{
+                      hidden: { opacity: 0 },
+                      visible: { opacity: 1 },
+                    }}
+                  >
+                    {val.title}
+                  </motion.h1>
+                )}
+              </AnimatePresence>
               <motion.div
                 initial="hidden"
                 animate="visible"
@@ -54,6 +82,18 @@ export default function TabComponent() {
           );
         })}
       </div>
-    </div>
+      <div className="px-4 absolute bottom-0 flex items-center justify-center gap-x-2 w-full border-t dark:border-t-neutral-800 text-sm font-semibold select-none h-15 text-center text-neutral-500">
+        <motion.div
+          onClick={toggleCollapse}
+          className="w-full"
+          whileTap={{ scale: 0.98 }}
+        >
+          <div className="transition-colors duration-300 dark:hover:bg-gray-800/40 rounded-2xl py-2 flex items-center justify-center gap-x-2 w-full">
+            <ChevronsLeft size={15} className={`${collapse && "rotate-180"}`} />
+            {!collapse && <span>Collapse</span>}
+          </div>
+        </motion.div>
+      </div>
+    </motion.div>
   );
 }
