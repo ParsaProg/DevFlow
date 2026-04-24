19c5bfad128afbfb499015053fd2ac8918ea6f47
ParsaProg <parsashaabani3@gmail.com>
2026-04-24 18:32:59 +0330

    Main page > dashboard

--- app/dashboard/projects/page.tsx
+++ app/dashboard/projects/page.tsx
@@ -1,13 +1,36 @@
+"use client";
+import { motion } from "framer-motion";
+import { useSidebar } from "@/src/context/DashBoardTabCollapseContext";
+import { Plus } from "lucide-react";
+import { ProjectsModelData } from "@/src/models/projects";
+import ProjectsContainer from "@/src/components/ui/projects/ProjectsContainer";
+
 const DashboardPage = () => {
+  const { isCollapsed } = useSidebar();
   return (
     <div className="z-10 absolute top-0 bg-[#05070B] w-full h-screen">
-      <div className="mt-24 ml-60 pl-15 flex flex-col gap-y-5">
-        <section>
-          <h1 className="text-white font-bold text-2xl">Good morning, Parsa</h1>
-          <h3 className="text-gray-500 font-thin text-[17px] mt-1">
-            Here is what is happening across your projects today.
-          </h3>
-        </section>
+      <div
+        className={`transition-all duration-300 mt-24 ${isCollapsed ? "ml-15" : "ml-60"} px-[12vw] flex flex-col mx-auto gap-y-5`}
+      >
+        <div className="flex w-full items-center justify-between">
+          <section>
+            <h1 className="text-white font-bold text-2xl">Projects</h1>
+            <h3 className="text-gray-500 font-thin text-[17px] mt-1">
+              6 active projects across your workspace
+            </h3>
+          </section>
+          <motion.div whileTap={{ scale: 0.95 }} className="rounded-xl">
+            <button className="bg-primary rounded-xl text-white font-semibold flex items-center justify-center gap-x-2 text-[13px] shadow-primary/20 shadow-[0px_0px_20px_5px] px-4 py-2">
+              <Plus size={18} />
+              Create Project
+            </button>
+          </motion.div>
+        </div>
+        <div className="grid lg:grid-cols-3 gap-5">
+          {ProjectsModelData.map((p, _i) => {
+            return <ProjectsContainer key={_i} {...p} />;
+          })}
+        </div>
       </div>
     </div>
   );
