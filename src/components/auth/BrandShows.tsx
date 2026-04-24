c0c0410dbb331b47eee61de109cf099422ad3e87
ParsaProg <parsashaabani3@gmail.com>
2026-04-24 18:34:17 +0330

    BrandShows.tsx > brandshows component

--- src/components/auth/BrandShows.tsx
+++ src/components/auth/BrandShows.tsx
@@ -2,17 +2,21 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import DevFlowLogo from "@/app/assets/pictures/devflow.png";
 
-export default function AuthPagesBrandShows() {
+export default function AuthPagesBrandShows({
+  showTitle = true,
+}: {
+  showTitle?: boolean;
+}) {
   return (
     <motion.div
-      transition={{ delay: 0.33 , ease: "anticipate"}}
+      transition={{ delay: 0.33, ease: "anticipate" }}
       initial="hidden"
       animate="visible"
       variants={{
         hidden: { opacity: 0, x: -100 },
         visible: { opacity: 1, x: 0 },
       }}
-      className="flex items-center gap-x-2 text-xl"
+      className="flex items-center gap-x-2 text-xl w-10 h-10"
     >
       <Image
         alt="DevFlow-Logo"
@@ -21,7 +25,7 @@ export default function AuthPagesBrandShows() {
         width={100}
         height={100}
       />
-      <h1>DevFlow</h1>
+      {showTitle && <h1>DevFlow</h1>}
     </motion.div>
   );
 }
