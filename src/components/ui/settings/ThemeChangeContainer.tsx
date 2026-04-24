c5feb4a481cf10a53c9e8418ebe8c506095e3cfe
ParsaProg <parsashaabani3@gmail.com>
2026-04-25 01:36:35 +0330

    ThemeChangeContainer added for more clean and more structured code.

---
+++ src/components/ui/settings/ThemeChangeContainer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useTheme } from "next-themes";
+
+export default function ThemeChangeAppearanceContainer({
+  text,
+}: {
+  text: string;
+}) {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div
+      onClick={() => setTheme(text)}
+      className={`gap-y-3 border w-full ring-2 select-none p-5 text-sm font-thin flex flex-col items-center rounded-xl ${theme === text.toLowerCase().toString() ? " ring-blue-500 border-transparent" : "dark:border-neutral-800 ring-transparent"}  `}
+    >
+      <div
+        className={`${text === "dark" ? "bg-[#0F1115]" : "bg-[#F5F5F7]"} w-full h-20 rounded-lg border dark:border-neutral-800`}
+      ></div>
+      <span>
+        {text[0].toUpperCase()}
+        {text.slice(1)}
+      </span>
+    </div>
+  );
+}
