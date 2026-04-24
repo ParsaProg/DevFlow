db2d16cb960002ac5ff17795139a3fc0e5b4ff49
ParsaProg <parsashaabani3@gmail.com>
2026-04-25 01:41:30 +0330

    Update a hover animation on the settings section appearcane theme change container or we can say button absolutly!

--- src/components/ui/settings/ThemeChangeContainer.tsx
+++ src/components/ui/settings/ThemeChangeContainer.tsx
@@ -11,7 +11,7 @@ export default function ThemeChangeAppearanceContainer({
   return (
     <div
       onClick={() => setTheme(text)}
-      className={`gap-y-3 border w-full ring-2 select-none p-5 text-sm font-thin flex flex-col items-center rounded-xl ${theme === text.toLowerCase().toString() ? " ring-blue-500 border-transparent" : "dark:border-neutral-800 ring-transparent"}  `}
+      className={`gap-y-3 border w-full ring-2 select-none p-5 text-sm font-thin flex flex-col items-center rounded-xl ${theme === text.toLowerCase().toString() ? " ring-blue-500 border-transparent" : "dark:border-neutral-800 ring-transparent hover:ring-primary/50 transition-all duration-200"}  `}
     >
       <div
         className={`${text === "dark" ? "bg-[#0F1115]" : "bg-[#F5F5F7]"} w-full h-20 rounded-lg border dark:border-neutral-800`}
