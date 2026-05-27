a79c422de8a9b13f61fd5aebe671f67a14a5b38c
ParsaProg <parsashaabani3@gmail.com>
2026-05-27 20:58:26 +0330

    DashboardSettings.tsx

--- src/components/settings/DashboardSettingsProfileInputForm.tsx
+++ src/components/settings/DashboardSettingsProfileInputForm.tsx
@@ -4,9 +4,13 @@ import "@/src/styles/inputs/dashboard.css";
 export default function DashboardSettingsProfileInputs({
   value,
   onChange,
+  placeholder,
+  height,
 }: {
   value: string;
   onChange?: (value: string) => void;
+  placeholder?: string;
+  height?: number;
 }) {
   const [inputValue, setInputValue] = useState(value);
 
@@ -15,11 +19,23 @@ export default function DashboardSettingsProfileInputs({
     onChange?.(e.target.value);
   };
 
-  return (
-    <input
+  return height ? (
+    <textarea
+      style={{
+        height: height,
+        resize: "none",
+      }}
+      placeholder={placeholder}
       value={inputValue}
       onChange={handleChange}
-      className="dashboard-input w-full dark:bg-[#05070B] border dark:border-neutral-800 rounded-2xl py-3 text-sm px-4 outline-none transition-all duration-200 ring-2 ring-transparent focus-within:ring-primary"
+      className={`placeholder:text-sm dashboard-input w-full dark:bg-[#05070B] border dark:border-neutral-800 rounded-2xl py-3 text-sm px-4 outline-none transition-all duration-200 ring-1 ring-transparent focus-within:ring-primary/80 shadow-transparent shadow-none focus-within:shadow-[0px_0px_7px_1px] focus-within:shadow-primary/50`}
+    />
+  ) : (
+    <input
+      placeholder={placeholder}
+      value={inputValue}
+      onChange={handleChange}
+      className={`placeholder:text-sm dashboard-input w-full dark:bg-[#05070B] border dark:border-neutral-800 rounded-2xl py-3 text-sm px-4 outline-none transition-all duration-200 ring-1 ring-transparent focus-within:ring-primary/80 shadow-transparent shadow-none focus-within:shadow-[0px_0px_7px_1px] focus-within:shadow-primary/50`}
     />
   );
 }
