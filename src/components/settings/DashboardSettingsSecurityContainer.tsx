f9db83d84197f15a2f2fb4ce31f12ed707070927
ParsaProg <parsashaabani3@gmail.com>
2026-05-28 04:12:45 +0330

    DashboardSettings.tsx

--- src/components/settings/DashboardSettingsSecurityContainer.tsx
+++ src/components/settings/DashboardSettingsSecurityContainer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import SecuritySettingsContainer from "../ui/settings/SecuritySettingsContainer";
+import SecuritySettingsContainer from "../ui/dashboard/settings/SecuritySettingsContainer";
 
 export default function DashboardSettingsSecurityContainer() {
   return (
